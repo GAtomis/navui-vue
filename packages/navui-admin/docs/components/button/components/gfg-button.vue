5be83ab46255408ac0c9445ec8f53ca3416e5428
GAtomis <850680822@qq.com>
2023-02-05 16:07:48 +0800

    fix : demoBlock

---
+++ packages/navui-admin/docs/components/button/components/gfg-button.vue
@@ -0,0 +1,27 @@
+
+<template>
+
+  <div class="container">
+    <nav-gfg-button>Add</nav-gfg-button>
+    <nav-gfg-button type="danger">Delete</nav-gfg-button>
+    <nav-gfg-button type="success">
+      <template #icon>
+        <SearchOutlined/>
+      </template>
+      {{ words }}
+    </nav-gfg-button>
+  </div>
+
+</template>
+<script lang="ts" setup>
+import { defineComponent, ref } from 'vue';
+import type {} from "../../../../build/dev-ui"
+ import {SearchOutlined} from '@ant-design/icons-vue'
+  const words= ref<string>("Sear dasds说")
+</script>
+<style scoped>
+.container {
+  display: flex;
+  perspective: 600px;
+}
+</style>
