26a6cfc18ae107d6b2b484acb2311b0900370b85
GAtomis <850680822@qq.com>
2023-02-13 00:52:43 +0800

    update : docs demo

--- packages/navui-env/src/main.ts
+++ packages/navui-env/src/main.ts
@@ -2,11 +2,10 @@
  * @Author: GAtomis 850680822@qq.com
  * @Date: 2023-01-16 23:00:02
  * @LastEditors: GAtomis
- * @LastEditTime: 2023-02-05 04:46:23
+ * @LastEditTime: 2023-02-12 21:16:48
  * @Description: 头部注释
  */
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import ui from "@library/navui-admin"
 import "@library/navui-admin/build/style.css"
